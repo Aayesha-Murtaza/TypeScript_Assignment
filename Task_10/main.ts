d700942477a951a5e9d0f7720c6844f723116c23
Aayesha-Murtaza <160833341+Aayesha-Murtaza@users.noreply.github.com>
2024-03-27 06:27:36 +0500

    first commit

---
+++ Task_10/main.ts
@@ -0,0 +1,9 @@
+/**Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does. */
+
+// Aayesha Murtaza
+// March 27, 2024
+// This program performs basic mathematic calculation which results in 8.
+console.log(7+1);
+console.log(10-2);
+console.log(72/9);
+console.log(4*2);
