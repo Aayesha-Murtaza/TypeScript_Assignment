c98583d170cdfeaa16e381c11c0a1c3059b3e0af
Aayesha-Murtaza <160833341+Aayesha-Murtaza@users.noreply.github.com>
2024-03-27 20:09:16 +0500

    first commit

---
+++ Task_23/main.ts
@@ -0,0 +1,45 @@
+/** Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test.*/
+
+let car = "Honda"; //Declares a varaible 'car' and assigns the value "Honda" to it.
+console.log("Is car == \"Honda\"? I predict true") // Prediction: true, since the variable car holds "Honda"
+console.log(car == "Honda"); //compare the value of variable 'car' to the string. 
+
+let flower = "Rose";
+console.log("\nIs flower == \"rose\"? I predict false")// Prediction: false, since the variable flower holds "Rose" (capital R)
+console.log(flower == "rose");
+
+let time = "4:14";
+console.log("\nIs time == \"4:14\"? I predict true")  // Prediction: true, since the variable time holds "4:14"
+console.log(time == "4:14");
+
+let ink = "blue";
+console.log("\nIs ink == \"black\"? I predict false") // Prediction: false, since the variable ink holds "blue"
+console.log(ink == "black");
+
+let hardwork = "success";
+console.log("\nIs hardwork == \"success\"? I predict true") // Prediction: true, since the variable hardwork holds "success"
+console.log(hardwork == "success");
+
+let year = "2024";
+console.log("\nIs year == \"2023\"? I predict false") // Prediction: false, since the variable year holds "2024"
+console.log(year == "2023");
+
+let day = "Thursday";
+console.log("\nIs day == \"Thursday\"? I predict true") // Prediction: true, since the variable day holds "Thursday"
+console.log(day == "Thursday");
+
+
+let js = "JavaScript";
+console.log("\nIs js == \"JavaSurf\"? I predict false") // Prediction: false, since the variable js holds "JavaScript"
+console.log(year == "JavaSurf");
+
+let ts = "TypeScript";
+console.log("\nIs ts == \"TypeScript\"? I predict true")  // Prediction: true, since the variable ts holds "TypeScript"
+console.log(ts == "TypeScript");
+
+let person = "tired";
+console.log("\nIs person == \"energetic\"? I predict false")  // Prediction: false, since the variable person holds "tired"
+console.log(year == "energetic");
+
+
+
