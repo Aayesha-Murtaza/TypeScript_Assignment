63fdbc5e91d2f2add17820ee6dd26995c02139b0
Aayesha-Murtaza <160833341+Aayesha-Murtaza@users.noreply.github.com>
2024-03-27 06:21:51 +0500

    first commit

---
+++ Task_9/main.js
@@ -0,0 +1,6 @@
+"use strict";
+/**Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message. */
+let favorite_num = 4;
+//create a message using the favorite_num and store it inside a variable 'message'
+var message = `My favorite number is ${favorite_num}. `;
+console.log(message);
---
+++ Task_9/main.ts
@@ -0,0 +1,7 @@
+/**Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message. */
+
+let favorite_num: number = 4;
+
+//create a message using the favorite_num and store it inside a variable 'message'
+var message: string = `My favorite number is ${favorite_num}. `;
+console.log(message);
