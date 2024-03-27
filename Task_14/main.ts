514214b0f9a53829af12de47456b284537036b23
Aayesha-Murtaza <160833341+Aayesha-Murtaza@users.noreply.github.com>
2024-03-27 06:52:16 +0500

    first commit

---
+++ Task_14/main.ts
@@ -0,0 +1,10 @@
+/**Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner. */
+
+
+var myArray2: string[] = ["Grandfather", "Mother", "Father"];
+
+// Loop through the array and print a message to each person
+for (let index = 0; index < myArray2.length; index++) {
+    console.log("Hello, " + myArray2[index] + "! you are invited to a family dinner on friday.");
+    
+}
