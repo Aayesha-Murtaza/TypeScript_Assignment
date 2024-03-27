66df3da026b11a5b289ece78ea3adfb6bb834c56
Aayesha-Murtaza <160833341+Aayesha-Murtaza@users.noreply.github.com>
2024-03-27 19:45:56 +0500

    first commit

---
+++ Task_21/main.js
@@ -0,0 +1,9 @@
+"use strict";
+/**Think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items. */
+let person = {
+    name: "Aayesha",
+    age: "18",
+    occupation: "Student"
+};
+//Log the person object to the console
+console.log(person);
---
+++ Task_21/main.ts
@@ -0,0 +1,11 @@
+/**Think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items. */
+
+let person = {
+    name: "Aayesha",
+    age: "18",
+    occupation: "Student"
+
+};
+
+//Log the person object to the console
+console.log(person);
